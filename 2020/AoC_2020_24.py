e5082e4228cb62bfa15614f587fdd117f7e785b2
Gerald van Dalen <geraldplus@gmail.com>
2025-12-03 05:51:50 +0000

    finished 2025 days 2 and 3

--- 2020/AoC_2020_24.py
+++ 2020/AoC_2020_24.py
@@ -39,52 +39,101 @@ def _(input_data):
     def parse_direction_string(s):
         return s.replace('se', 'se,').replace('sw', 'sw,').replace('nw', 'nw,').replace('ne', 'ne,') \
                 .replace('ws', 'w,s').replace('wn', 'w,n').replace('es', 'e,s').replace('en', 'e,n') \
-                .replace('ee', 'e,e').replace('ww', 'w,w').replace('ew', 'e,w').replace('we', 'w,e') \
-                .strip(',').split(',')
+                .replace('ew', 'e,w').replace('we', 'w,e').replace('ee', 'e,e').replace('ww', 'w,w') \
+                .replace('ee', 'e,e').replace('ww', 'w,w').strip(',').split(',')
+
+    def norm_tile_tuple(tile):
+        min_ns = min(tile[0], tile[2])
+        min_ew = min(tile[1], tile[3])
+        return (tile[0]-min_ns, tile[1]-min_ew, tile[2]-min_ns, tile[3]-min_ew)
+
+    def tile2tuple(tile):
+        '''Convert dict to (n, e, s, w)'''
+        return norm_tile_tuple((tile['n'], tile['e'], tile['s'], tile['w']))
 
     def netto_move(moves):
-        dir_count = {}
+        dir_count = {'e': 0, 'n': 0, 's': 0, 'w': 0}
         for m in moves:
             for d in m:
-                if d not in dir_count.keys():
-                    dir_count[d] = 0
-                dir_count[d] += 1
-    
-        # dir_count = {k:len([1 for m in moves if m == k]) for k in set(moves)}
-        # pairs = [('se', 'nw'), ('sw', 'ne'), ('e', 'w')]
+                dir_count[d] += 2//len(m)
         pairs = [('n', 's'), ('e', 'w')]
         for p1, p2 in pairs:
-            if p1 in dir_count.keys() and p2 in dir_count.keys():
-                min_p1p2 = min(dir_count[p1], dir_count[p2])
-                dir_count[p1] -= min_p1p2
-                dir_count[p2] -= min_p1p2
-                # if dir_count[p1] == 0:
-                #     del dir_count[p1]
-                # if dir_count[p2] == 0:
-                #     del dir_count[p2]
-        return ','.join([f"{k}:{v}" for k, v in sorted(dir_count.items(), key=lambda l: l[0])])
-    
+            min_p1p2 = min(dir_count[p1], dir_count[p2])
+            dir_count[p1] -= min_p1p2
+            dir_count[p2] -= min_p1p2
+        return dir_count
+
 
     def problem_a(data):
         tiles = {}
         for d in data:
             moves = parse_direction_string(d)
-            # print(moves)
             nm = netto_move(moves)
-            if nm not in tiles.keys():
-                tiles[nm] = 0
-            tiles[nm] += 1
+            hash_nm = tile2tuple(nm)
+            if hash_nm not in tiles.keys():
+                tiles[hash_nm] = 0
+            tiles[hash_nm] += 1
         print(tiles)
-        print([v for v in tiles.values()])
-        return None
+        return sum([i%2 for i in tiles.values()])
     answer_a = problem_a(input_data)
-    return (answer_a,)
+    return (
+        answer_a,
+        netto_move,
+        norm_tile_tuple,
+        parse_direction_string,
+        tile2tuple,
+    )
 
 
 @app.cell
-def _(input_data):
+def _(
+    input_data,
+    netto_move,
+    norm_tile_tuple,
+    parse_direction_string,
+    tile2tuple,
+):
+    def get_neighbours(tile):
+        '''Return neighbour tiles: nw (n+1, w+1), ne (n+1, e+1), sw (s+1, w+1), se (s+1, e+1), e (e+2), w (w+2)'''
+        return [
+            norm_tile_tuple((tile[0]+1, tile[1], tile[2], tile[3]+1)),
+            norm_tile_tuple((tile[0]+1, tile[1]+1, tile[2], tile[3])),
+            norm_tile_tuple((tile[0], tile[1], tile[2]+1, tile[3]+1)),
+            norm_tile_tuple((tile[0], tile[1]+1, tile[2]+1, tile[3])),
+            norm_tile_tuple((tile[0], tile[1]+2, tile[2], tile[3])),
+            norm_tile_tuple((tile[0], tile[1], tile[2], tile[3]+2))
+        ]
+    
+
     def problem_b(data):
-        # TODO solve problem b
+        # Short way to do problem a again (True is black, False is white)
+        tiles = {}
+        for d in data:
+            tile = tile2tuple(netto_move(parse_direction_string(d)))
+            tiles[tile] = True if tile not in tiles.keys() else not(tiles[tile])
+        print(sum(tiles.values()))
+
+        # Simulate 100 days
+        for day in range(10):
+            new_tiles = tiles.copy()
+            for t in tiles.keys():
+                # Get neighbours and count black ones. Also, add to tiles if not added yet.
+                neighbours = get_neighbours(t)
+                neighbours_black_count = 0
+                for n in neighbours:
+                    if n not in tiles.keys():
+                        new_tiles[n] = False
+                    if new_tiles[n]:
+                        neighbours_black_count += 1
+                # Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white.
+                if tiles[t] and (neighbours_black_count == 0 or neighbours_black_count > 2):
+                    new_tiles[t] = not(tiles[t])
+                # Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
+                if not tiles[t] and neighbours_black_count == 2:
+                    new_tiles[t] = not(tiles[t])
+            tiles = new_tiles.copy()
+            print(f"Day {day+1}: {sum(tiles.values())}")
+    
         return None
     answer_b = problem_b(input_data)
     return (answer_b,)
@@ -95,6 +144,25 @@ def _(answer_a, answer_b, day_number):
     # Show answers
     print(f"Day {int(day_number)}a: {answer_a if answer_a else '-'}")
     print(f"Day {int(day_number)}b: {answer_b if answer_b else '-'}")
+    return
+
+
+@app.cell
+def _():
+    a = [True, True, False, True]
+    sum(a)
+    return
+
+
+@app.cell
+def _():
+    b = False if False else not(False)
+    return (b,)
+
+
+@app.cell
+def _(b):
+    b
     return
 
 
---
+++ 2025/AoC_2025_02.py
@@ -0,0 +1,92 @@
+import marimo
+
+__generated_with = "0.18.0"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    # Import statements
+    import os
+    import numpy as np
+    import math
+    import re
+
+    # Settings
+    sample = False # Fill in False, or the sample number (True and 1 are the same)
+    return os, sample
+
+
+@app.cell
+def _(os, sample):
+    # Get problem input
+    day_number = os.path.basename(__file__).split(sep=".")[0].split(sep="_")[-1]
+    def post_process(data):
+        # Problem-specific post-processing
+        data = data[0].strip().split(",")
+        data = [(int(d.split("-")[0]), int(d.split("-")[1])) for d in data]
+        print(data)
+        return data
+
+    def load_input(sample=False):
+        curdir = "/".join(os.path.abspath(__file__).split("/")[:-1]) + "/"
+        filename = curdir + (f"input_{day_number}_sample{'_'+str(sample) if int(sample)>1 else ''}.txt" if sample else f"input_{day_number}.txt")
+        return post_process(open(filename, "r").readlines())
+
+    input_data = load_input(sample)
+    return day_number, input_data
+
+
+@app.cell
+def _(input_data):
+    def get_all_nums(ranges):
+        '''Create flattened list of all numbers in all ranges'''
+        all_nums = [list(range(d[0], d[1]+1)) for d in ranges]
+        return [n for sublist in all_nums for n in sublist]
+
+    def problem_a(data):
+        '''A number is invalid if the first half of the digits gives the same number as the second half.
+        Output: sum of the invalid numbers.'''
+        sum_invalid = 0
+        for n in get_all_nums(data):
+            s = str(n)
+            # Check length of digits is even and first half of digits equals last half
+            if len(s) % 2 == 0 and s[:len(s)//2] == s[len(s)//2:]:
+                sum_invalid += n
+        return sum_invalid
+    answer_a = problem_a(input_data)
+    return answer_a, get_all_nums
+
+
+@app.cell
+def _(get_all_nums, input_data):
+    def problem_b(data):
+        '''A number is invalid if all digits consists of a repeated pattern of digits.
+        Output: sum of the invalid numbers.'''
+        sum_invalid = 0
+        for n in get_all_nums(data):
+            s = str(n)
+            for i in range(2, len(s)+1):
+                if len(s) % i == 0 and s[:len(s)//i]*i == s:
+                    sum_invalid += n
+                    break
+        return sum_invalid
+    answer_b = problem_b(input_data)
+    return (answer_b,)
+
+
+@app.cell
+def _(answer_a, answer_b, day_number):
+    # Show answers
+    print(f"Day {int(day_number)}a: {answer_a if answer_a else '-'}")
+    print(f"Day {int(day_number)}b: {answer_b if answer_b else '-'}")
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
